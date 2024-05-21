9c5e144fc762c061f4867752dd05c6860219e8c6
Abdus <abdus@abdus.net>
2024-05-21 18:19:26 +0530

    feat: new team section

--- src/lib/data/team.ts
+++ src/lib/data/team.ts
@@ -6,6 +6,11 @@ import PhotoAbdus from '../../assets/images/photos/abdus.webp';
 import PhotoSumedha from '../../assets/images/photos/sumedha.webp';
 import PhotoSonali from '../../assets/images/photos/sonali.webp';
 import PhotoHritik from '../../assets/images/photos/hritik.webp';
+import PhotoTuriya from '../../assets/images/photos/turiya.webp';
+import PhotoPriyansha from '../../assets/images/photos/priyansha.webp';
+import PhotoSachin from '../../assets/images/photos/sachin.webp';
+import PhotoGiftin from '../../assets/images/photos/giftin.webp';
+import PhotoShreya from '../../assets/images/photos/shreya.webp';
 
 export const teamMembers = [
 	{ name: 'Priyanka Joshi', role: 'Chairman/Co-Founder', image: PhotoPriyanka },
@@ -13,15 +18,12 @@ export const teamMembers = [
 	{ name: 'Aaditya Joshi', role: 'Co-Founder', image: PhotoAaditya },
 	{ name: 'Aayush Anand', role: 'Evangelist', image: PhotoAayush },
 	{ name: 'Abdus Azad', role: 'Tech Evangelist', image: PhotoAbdus },
-	{
-		name: 'Sumedha Upadhyay',
-		role: 'Growth & Strategy Lead',
-		image: PhotoSumedha
-	},
+	{ name: 'Sumedha Upadhyay', role: 'Growth & Strategy Lead', image: PhotoSumedha },
 	{ name: 'Sonali Bhatnagar', role: 'Mental Health Expert', image: PhotoSonali },
-	{
-		name: 'Hrithik S. Naudiyal',
-		role: 'Social Media & PR Strategist',
-		image: PhotoHritik
-	}
+	{ name: 'Hrithik S. Naudiyal', role: 'Social Media & PR Strategist', image: PhotoHritik },
+	{ name: 'Giftin Wilson', role: 'Fitness Strategist', image: PhotoGiftin },
+	{ name: 'Turiya Kapoor', role: 'Social Media Strategist', image: PhotoTuriya },
+	{ name: 'Priyansha Gupta', role: 'Growth & Strategy', image: PhotoPriyansha },
+	{ name: 'Sachin Sati', role: 'Social Media Strategist', image: PhotoSachin },
+	{ name: 'Shreya Jain', role: 'Growth Strategist', image: PhotoShreya },
 ];
