911e776957ae334eb8a1482b47d8303a117e2179
Abdus <abdus@abdus.net>
2024-05-21 17:26:04 +0530

    fix: (minor) layout

--- src/lib/components/pdf-viewer.svelte
+++ src/lib/components/pdf-viewer.svelte
@@ -16,9 +16,9 @@
 	const images = [Png1, Png2, Png3, Png4, Png5, Png6, Png7, Png8, Png9];
 </script>
 
-<div class="max-w-[60rem] mx-auto">
-	<Heading title="From the Doctors" subtitle="" />
+<Heading title="From the Doctors" subtitle="" />
 
+<div class="max-w-[60rem] mx-auto">
 	<Splide
 		aria-label="Story of Rekha Joshi"
 		hasTrack={false}
