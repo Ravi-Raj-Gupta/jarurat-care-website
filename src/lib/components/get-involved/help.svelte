a139b0d2d7a59a9a3da552aef84e2ab7db842a8d
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-09-15 20:00:52 +0530

    Refactor help section component and adjust button spacing

--- src/lib/components/get-involved/help.svelte
+++ src/lib/components/get-involved/help.svelte
@@ -4,11 +4,11 @@
             <span class="text-[#0155BD]">HELP OTHERS</span>
         </h2>
         <div class="my-4 text-center">"Thank you for Choosing to Make a Difference"</div>
-        <p class="w-[80%] overflow-hidden text-ellipsis  text-wrap sm:text-sm lg:font-normal box-border">Your commitment to volunteering with Jarurat Care is invaluable. By offering your time and talents, you’re playing a crucial role in bringing hope and support to those who need it most. We deeply appreciate your willingness to join us in our mission to create positive change. Your contributions help us extend our reach, provide essential services, and touch countless lives. Thank you for being a part of our community and for making a lasting impact through your generosity and kindness.</p>
+        <p class="w-[50%] overflow-hidden text-ellipsis  text-wrap text-[0.7em] md:text-sm lg:font-normal box-border">Your commitment to volunteering with Jarurat Care is invaluable. By offering your time and talents, you’re playing a crucial role in bringing hope and support to those who need it most. We deeply appreciate your willingness to join us in our mission to create positive change. Your contributions help us extend our reach, provide essential services, and touch countless lives. Thank you for being a part of our community and for making a lasting impact through your generosity and kindness.</p>
         <div class="buttons my-5 flex md:flex-row flex-col ">
-            <button class="bg-[#9CCB44] text-white md:py-2 md:px-4 px-2 py-1 my-1 border-[2px] border-double border-[#c2ec8b]  rounded-full md:my-4  md:mx-3"><p class="text-sm md:font-normal">Volunteer</p></button>
-            <button class="bg-[#0155BD] text-white md:py-2 md:px-4 px-2 py-1 my-1 border-[2px] border-double border-[#78a8e2] rounded-full md:my-4  md:mx-3"><p class="text-sm md:font-normal">Join Our Team</p></button>
-            <button class="bg-[#FFBA41] text-white md:py-2 md:px-4 px-2 py-1 my-1 border-[2px] border-double border-[#efc171] rounded-full md:my-4  md:mx-3"><p class="text-sm md:font-normal">Partnership</p></button>
+            <button class="bg-[#9CCB44] text-white md:py-2 md:px-4 px-2 py-1 my-1 border-[2px] border-double border-[#c2ec8b]  rounded-full md:my-4  md:mx-1"><p class="text-sm md:font-normal">Volunteer</p></button>
+            <button class="bg-[#0155BD] text-white md:py-2 md:px-4 px-2 py-1 my-1 border-[2px] border-double border-[#78a8e2] rounded-full md:my-4  md:mx-1"><p class="text-sm md:font-normal">Join Our Team</p></button>
+            <button class="bg-[#FFBA41] text-white md:py-2 md:px-4 px-2 py-1 my-1 border-[2px] border-double border-[#efc171] rounded-full md:my-4  md:mx-1"><p class="text-sm md:font-normal">Partnership</p></button>
         </div>
     </div>
 </div>
