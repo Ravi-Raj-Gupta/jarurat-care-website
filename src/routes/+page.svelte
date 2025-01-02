dd886a1b64a13b7e0d7f88960e757bd4e8cca845
RajeshRajamanickam <rajesh.r718@gmail.com>
2025-01-02 08:24:34 +0530

    add seo to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,28 @@
 <script>
-	import Blogs from '$lib/components/blogs.svelte';
-	import CancerResearch from '$lib/components/cancer-research.svelte';
-	import Faq from '$lib/components/faqs/faq.svelte';
-	import Hero from '$lib/components/hero.svelte';
-	import HowWeSupportYou from '$lib/components/how-we-support-you.svelte';
-	import Impact from '$lib/components/impact.svelte';
-	import MapOfHospitals from '$lib/components/map-of-hospitals.svelte';
-	import MonthlyUpdates from '$lib/components/monthly-updates.svelte';
-	import Nav from '$lib/components/nav.svelte';
-	import Newsletter from '$lib/components/newsletter.svelte';
-	import OurTeam from '$lib/components/our-team.svelte';
-	import SupportDonateMember from '$lib/components/support-donate-member.svelte';
-	import VoiceOfHope from '$lib/components/voice-of-hope.svelte';
-	import WhoWeAre from '$lib/components/who-we-are.svelte';
+    import Blogs from '$lib/components/blogs.svelte';
+    import CancerResearch from '$lib/components/cancer-research.svelte';
+    import Faq from '$lib/components/faqs/faq.svelte';
+    import Hero from '$lib/components/hero.svelte';
+    import HowWeSupportYou from '$lib/components/how-we-support-you.svelte';
+    import Impact from '$lib/components/impact.svelte';
+    import MapOfHospitals from '$lib/components/map-of-hospitals.svelte';
+    import MonthlyUpdates from '$lib/components/monthly-updates.svelte';
+    import Nav from '$lib/components/nav.svelte';
+    import Newsletter from '$lib/components/newsletter.svelte';
+    import OurTeam from '$lib/components/our-team.svelte';
+    import SupportDonateMember from '$lib/components/support-donate-member.svelte';
+    import VoiceOfHope from '$lib/components/voice-of-hope.svelte';
+    import WhoWeAre from '$lib/components/who-we-are.svelte';
+
+    // Google Analytics initialization
+    if (typeof window !== 'undefined') {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-SR1C89YNC3');
+    }
 </script>
 
 <Nav />
@@ -29,3 +39,8 @@
 <Blogs />
 <Faq />
 <Newsletter />
+
+<!-- Add the script asynchronously in the document -->
+<svelte:head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SR1C89YNC3"></script>
+</svelte:head>
